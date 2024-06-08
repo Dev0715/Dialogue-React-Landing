14da2dbd492559a730dce63cd14f481e6a0aaf96
dev0715 <webdev0715@gmail.com>
2024-06-09 04:51:10 +0900

    update: fix to match design

--- src/components/landing/features/feature/index.js
+++ src/components/landing/features/feature/index.js
@@ -2,7 +2,7 @@ import styles from "./styles.module.css";
 
 const Feature = ({ image, title, description, left }) => {
   return (
-    <div className={styles.feature}>
+    <div className={`${styles.feature} ${!left ? styles.feature_left : null}`}>
       <div className={styles.border_top}></div>
       {left ? <div className={styles.border_right}></div> : null}
       <img className={styles.image} src={image} alt={title} />
