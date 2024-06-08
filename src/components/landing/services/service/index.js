7b8b05a7f6ff2b4b9d4ada54d1f87fb3f169e8b8
dev0715 <webdev0715@gmail.com>
2024-06-09 06:13:23 +0900

    update: divider extent

--- src/components/landing/services/service/index.js
+++ src/components/landing/services/service/index.js
@@ -3,11 +3,13 @@ import styles from "./styles.module.css";
 const Service = ({ src, image_description, title, description }) => {
   return (
     <div className={styles.service}>
-      <div className={styles.splitter} />
-      <img className={styles.image} src={src} alt="information" />
-      <p className={styles.image_description}>{image_description}</p>
-      <p className={styles.title}>{title}</p>
-      <p className={styles.description}>{description}</p>
+      <div className={styles.wrapper}>
+        <div className={styles.splitter} />
+        <img className={styles.image} src={src} alt="information" />
+        <p className={styles.image_description}>{image_description}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
