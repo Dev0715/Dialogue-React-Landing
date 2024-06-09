4e1c025f930e1d8b722799cea399ae879321832b
dev0715 <webdev0715@gmail.com>
2024-06-09 20:18:03 +0900

    fix: svg and png

--- src/components/landing/services/index.js
+++ src/components/landing/services/index.js
@@ -4,14 +4,16 @@ import Service from "./service";
 const Services = () => {
   const services = [
     {
-      src: "/images/information-resolution.png",
+      src_desktop: "/images/information-resolution.svg",
+      src_mobile: "/images/information-resolution.png",
       image_description: "Information resolution scaling",
       title: "Get to the story behind the story in the blink of an eye",
       description:
         "Dialogue presents information across Views. Instantly surface section summaries, core ideas and references. Get faster to what you find interesting.",
     },
     {
-      src: "/images/conversational.png",
+      src_desktop: "/images/conversational.svg",
+      src_mobile: "/images/conversational.png",
       image_description: "Conversational text editing & image creation",
       title:
         "Halve the time needed to communicate and illustrate your thoughts",
@@ -19,7 +21,8 @@ const Services = () => {
         "Dialogue integrates and analyses your knowledge bases. Bring your work to life with personalized editing suggestions and AI generated visual content.",
     },
     {
-      src: "/images/interactive-knowledge.png",
+      src_desktop: "/images/interactive-knowledge.svg",
+      src_mobile: "/images/interactive-knowledge.png",
       image_description: "Interactive knowledge network graphs",
       title: "Find people with complementary skillsets and expertise",
       description:
@@ -32,7 +35,8 @@ const Services = () => {
       {services.map((item, index) => (
         <Service
           key={index}
-          src={item.src}
+          src_desktop={item.src_desktop}
+          src_mobile={item.src_mobile}
           image_description={item.image_description}
           title={item.title}
           description={item.description}
--- src/components/landing/services/service/index.js
+++ src/components/landing/services/service/index.js
@@ -1,11 +1,26 @@
 import styles from "./styles.module.css";
 
-const Service = ({ src, image_description, title, description }) => {
+const Service = ({
+  src_desktop,
+  src_mobile,
+  image_description,
+  title,
+  description,
+}) => {
   return (
     <div className={styles.service}>
       <div className={styles.wrapper}>
         <div className={styles.splitter} />
-        <img className={styles.image} src={src} alt="information" />
+        <img
+          className={styles.image_desktop}
+          src={src_desktop}
+          alt="information"
+        />
+        <img
+          className={styles.image_mobile}
+          src={src_mobile}
+          alt="information"
+        />
         <p className={styles.image_description}>{image_description}</p>
         <p className={styles.title}>{title}</p>
         <p className={styles.description}>{description}</p>
